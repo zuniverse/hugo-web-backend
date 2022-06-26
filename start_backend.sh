6c9dc645a6be6bee6c8f5540087339fe5497382b
Paul Zuliani <paulzuliani@gmail.com>
2022-06-26 13:10:19 +0200

    add start backend script to replace makefile

---
+++ start_backend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# -x : Print a trace of simple commands
+# -e : Exit immediately if a pipeline returns a non-zero status.
+set -ex
+
+source "venv/bin/activate"
+
+# check i'm in good venv
+echo $VIRTUAL_ENV
+
+flask run
